f01a1286c4423f1594a45636c128a0fa216b35db
Yoni <firroloni.yoni@gmail.com>
2021-05-11 15:21:51 +0200

    add error message

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ fn base64decode(input: String) -> String {
                 'A' ..= 'Z' => ascii + UPPERCASE_OFFSET,
                 '+' => 62,
                 '/' => 63,
-                _ => 20
+                _ => panic!("Unexpected character: {}", ch)
             };
             format!("{:#08b}", convert)[2..].to_string()        //convert indices to binary format and remove the two first digits
         })

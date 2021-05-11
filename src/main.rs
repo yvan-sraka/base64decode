16b08af3b75fb2a9ccbd9f1bf77292ab244d8c05
JeremTer <ternisien09@gmail.com>
2021-05-11 10:40:49 +0200

    Make main.rs work

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::str;
- 
+
 const UPPERCASE_OFFSET: i8 = -65;
 const LOWERCASE_OFFSET: i8 = 26 - 97;
 const NUM_OFFSET: i8 = 52 - 48;
@@ -8,13 +8,14 @@ fn base64decode(input: String) -> String {
     let result = input.trim().chars()
         .filter(|&ch| ch != '=')                                //Filter '=' chars
         .map(|ch| {                                             //Map char values using Base64 Characters Table
-            let ascii = ch as i8;                           
+            let ascii = ch as i8;
             let convert = match ch {
-                '0' ..= '9' => ascii + NUM_OFFSET,
-                'a' ..= 'z' => ascii + LOWERCASE_OFFSET,
-                'A' ..= 'Z' => ascii + UPPERCASE_OFFSET,
+                '0'..='9' => ascii + NUM_OFFSET,
+                'a'..='z' => ascii + LOWERCASE_OFFSET,
+                'A'..='Z' => ascii + UPPERCASE_OFFSET,
                 '+' => 62,
-                '/' => 63
+                '/' => 63,
+                _ => 0
             };
             format!("{:#08b}", convert)[2..].to_string()        //convert indices to binary format and remove the two first digits
         })
@@ -27,16 +28,16 @@ fn base64decode(input: String) -> String {
             usize::from_str_radix(&num_str, 2).unwrap() as u8   //convert the binary string into its u8 value
         })
         .collect::<Vec<_>>();
- 
+
     let result = str::from_utf8(&result).unwrap();              //convert into UTF-8 string
     return String::from(result);
 }
- 
+
 fn main() {
-    let input = String::new();
-    std::io::stdin().read_line(input).unwrap();
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
     println!("Input: {}", input);
- 
+
     let output = base64decode(input);
     println!("Output: {}", output);
 }
